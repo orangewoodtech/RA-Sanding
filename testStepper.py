17bfcfe5b694b3078dc00c3d889f16a89135d2f7
sidharthbhatia-orangewood <sidharth.b@orangewood.co>
2018-09-13 17:01:44 +0530

    Complete the RA-sanding inverse kinematics equation´s RPI code

--- testStepper.py
+++ testStepper.py
@@ -24,9 +24,9 @@ l1=42
 l2=36
 ######## Coordinates in xy frame in cm
 
-x = 10
-y = 62
-z = 0
+ox = 17.8
+oy = 18.4
+oz = 0
 
 #######Inverse Kinematics Equation for obtaining th joint angles -
 
@@ -34,15 +34,27 @@ z = 0
 ##theta2=math.degrees(math.atan((y*(l1+l2*math.cos(theta3))-(x*l2*math.sin(theta3)))/(x*(l1+l2*math.cos(theta3))+(y*l2*math.sin(theta3)))))
 ##theta1=math.degrees(math.atan(z/x))
 
-theta3 = 0
-theta2 = 0
-theta1 = 0
+##theta3 = 0
+##theta2 = 0
+##theta1 = 0
+##
+##theta1=math.degrees(math.acos(-(x*x+y*y+(l1*l1)-(l2*l2))/ (2*l1* math.sqrt(x*x + y*y))) + math.atan(y/x))  
+##theta2=math.degrees(math.atan((y-l1*math.sin(theta1))/(x- l1*math.cos(theta1))) - (theta1))
+##theta3=math.degrees(math.acos(z/(l1*math.cos(theta1)+l2*math.cos((theta1)+(theta2)))))
 
-theta2=math.degrees(math.acos((x*x+y*y-(l1*l1)-(l2*l2))/(2*l1*l2)))
-theta3=math.degrees(math.atan((y*(l1+l2*math.cos(theta2))-(x*l2*math.sin(theta2)))/(x*(l1+l2*math.cos(theta2))+(y*l2*math.sin(theta2)))))
-theta1=math.degrees(math.atan(z/x))
+oldtheta2=-math.degrees(math.acos((ox*ox+oy*oy-(l1*l1)-(l2*l2))/ (2*l1*l2)))  
+oldtheta1=math.degrees(math.atan(oy/ox) - math.atan((l2*math.sin(oldtheta2*math.pi/180))/(l1 + l2*math.cos(oldtheta2*math.pi/180))))
+oldtheta3=math.degrees(math.acos(oz/(l1*math.cos(oldtheta1*math.pi/180) + l2*math.cos((oldtheta2 + oldtheta1)*math.pi/180))))
 
-print(str(theta1)+" "+str(theta2)+ " "+str(theta3))
+##theta2=math.degrees(math.acos((x*x+y*y-(l1*l1)-(l2*l2))/(2*l1*l2)))
+##theta3=math.degrees(math.atan((y*(l1+l2*math.cos(theta2))-(x*l2*math.sin(theta2)))/(x*(l1+l2*math.cos(theta2))+(y*l2*math.sin(theta2)))))
+##theta1=math.degrees(math.atan(z/x))
+
+##theta2=-math.degrees(math.atan(math.sqrt((2*l1*l2 - math.pow((x*x + y*y - l1*l1 - l2*l2), 2))/(x*x + y*y - l1*l1 - l2*l2))))  
+##theta1=math.degrees(math.atan(y/x) - math.atan((l2*math.sin(theta2*math.pi/180))/(l1 + l2*math.cos(theta2*math.pi/180))))
+##theta3=math.degrees(math.acos(z/(l1*math.cos(theta1*math.pi/180) + l2*math.cos((theta2 + theta1)*math.pi/180))))
+
+print(str(oldtheta1)+" oldtheta2:"+str(oldtheta2)+ " oldtheta3:"+str(oldtheta3))
 
 ## Creation of objects for various motors
 ##testStepper1 = stepper(s1)
@@ -51,20 +63,31 @@ print(str(theta1)+" "+str(theta2)+ " "+str(theta3))
 
 ppr=1600  # Pulse Per Revolution
 
-# angles to be moved
-a1=theta1  #base
-a2=theta2  #link 1
-a3=theta3  #link 2
+x = 17.8
+y = 18.4
+z = 0
 
-##a1=30  #base
-##a2=40  #link 1
-##a3=30  #link 2
+theta2=-math.degrees(math.acos((x*x+y*y-(l1*l1)-(l2*l2))/ (2*l1*l2)))  
+theta1=math.degrees(math.atan(y/x) - math.atan((l2*math.sin(theta2*math.pi/180))/(l1 + l2*math.cos(theta2*math.pi/180))))
+theta3=math.degrees(math.acos(z/(l1*math.cos(theta1*math.pi/180) + l2*math.cos((theta2 + theta1)*math.pi/180))))
+
+# angles to be moved
+a1=theta3 - oldtheta3 #base
+a2=theta1 - oldtheta1 #link 1
+a3=theta2 - oldtheta2 #link 2
+
+print(str(theta1)+" theta2:"+str(theta2)+ " theta3:"+str(theta3))
+print(str(a1)+" a2:"+str(a2)+ " a3:"+str(a3))
+
+##a1=0  #base
+##a2=-39  #link 1
+##a3=0  #link 2
 
 ## Gear Ratios
 g1=12.2
 g2=10
 g3=10
-
+ 
 # Calculation for step and Speed
 step1=(ppr/360)*a1*g1  
 #speed1=0.01
@@ -120,6 +143,6 @@ if step3<0:
 else:
     dir3="r"
 
-_thread.start_new_thread( print_time, ("stepper-1", 0.2, s1,abs(step1),"l",td1))
-_thread.start_new_thread( print_time, ("stepper-2", 0.2, s2,abs(step2),"l",td2))
-_thread.start_new_thread( print_time, ("stepper-3", 0.2, s3,abs(step3),"r",td3))
+_thread.start_new_thread( print_time, ("stepper-1", 0.2, s1,abs(step1),dir1,td1))
+_thread.start_new_thread( print_time, ("stepper-2", 0.2, s2,abs(step2),dir2,td2))
+_thread.start_new_thread( print_time, ("stepper-3", 0.2, s3,abs(step3),dir3,td3)) 
